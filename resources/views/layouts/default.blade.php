488dd3654d359d65f1323234bf343f49e0609e6f
vagrant <vagrant@homestead>
2020-02-23 12:45:01 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,12 +4,18 @@
  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
  </head>
+
+
  <body>
  @include('layouts._header')
+
  <div class="container">
  <div class="offset-md-1 col-md-10">
- @yield('content')
- @include('layouts._footer')
+    @include('shared._messages')
+
+    @yield('content')
+
+    @include('layouts._footer')
  </div>
  </div>
  </body>
