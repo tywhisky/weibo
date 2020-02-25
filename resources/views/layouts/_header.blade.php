ad3bbe2b0ba37acbdd535f6856ce7cbf9880c387
vagrant <vagrant@homestead>
2020-02-25 06:17:51 +0000

    更改用户资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,8 +11,9 @@
  {{ Auth::user()->name }}
  </a>
 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
- <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心
- <a class="dropdown-item" href="#">编辑资料</a>
+ <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
+ <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">编辑资料</a>
+
  <div class="dropdown-divider"></div>
  <a class="dropdown-item" id="logout" href="#">
  <form action="{{ route('logout') }}" method="POST">
